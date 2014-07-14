4619340cd0fac73c6756fea2d110a258e85ce648
wfsiew <siewwingfei@hotmail.com>
2014-07-14 23:22:48 +0800

    c

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using ConsoleApplication2.Models;
 
 namespace ConsoleApplication2
 {
@@ -10,6 +13,15 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            string path = "..\\..\\fibre+\\external.xml";
+            var d = FibrePlusExternal.LoadList(path);
+
+            foreach (KeyValuePair<int, FibrePlusExternal> k in d)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", k.Value.Type, k.Value.Commission, k.Value.Tier1, k.Value.Tier2, k.Value.Tier3);
+            }
+
+            Console.ReadKey();
         }
     }
 }
